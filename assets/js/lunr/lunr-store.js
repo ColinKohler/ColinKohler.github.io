e42f34ae99474b7a93c51b24c09d7aa09c97b8e1
ColinKohler <ColinKohler@users.noreply.github.com>
2023-08-24 17:29:07 +0000

    jekyll build from Action 4ff3bec296bc122c2371381d7b228c5c1b326e76

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,37 @@
+var store = [{
+        "title": "Deictic Image Maps: An Abstraction For Learning Pose Invariant Manipulation Policies",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/research/deictic_image_maps/",
+        "teaser": null
+      },{
+        "title": "Towards Assistive Robotic Pick and Place in Open World Environments",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/research/robotic_scooter/",
+        "teaser": null
+      },{
+        "title": "Policy learning in SE(3) action spaces",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/research/se2_policy_learning/",
+        "teaser": null
+      },{
+        "title": "BulletArm: An Open-Source Robotic Manipulation Benchmark and Learning Framework",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/research/BulletArm/",
+        "teaser": null
+      },{
+        "title": "Visual Foresight with a Local Dynamics Model",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/research/visual_foresight/",
+        "teaser": null
+      },{
+        "title": "Symmetric Models for Visual Force Policy Learning",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/research/symmetric_visual_force_learning/",
+        "teaser": null
+      }]
